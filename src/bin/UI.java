682853876027e22fe098f140eebe1cd642d26d82
utmar02 <utmar02@TXIH-1V3ZLC2.txfb-ins.com>
2020-02-26 19:20:19 -0600

    First commit!

--- src/bin/UI.java
+++ src/bin/UI.java
@@ -3,7 +3,7 @@ public class UI  {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		XMLTestesView cont = new XMLTestesView();
-		 
+		XMLTestesView view = new XMLTestesView();
+		
 	}
 }
